a1d7129d795799c1a20798ee51c7477196dfd932
Matt <Matthew.hung01@gmail.com>
2022-06-16 23:13:13 -0400

    add public folder

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express();
 const PORT = 8000;
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://Matthung:EINZm8oa26LRSVSl@cluster0.t7zaa.mongodb.net/?retryWrites=true&w=majority';
 
-
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
   }));
 
-MongoClient.connect('mongodb+srv://Matthung:EINZm8oa26LRSVSl@cluster0.t7zaa.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+MongoClient.connect(uri, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('pasta');
@@ -21,20 +22,8 @@ MongoClient.connect('mongodb+srv://Matthung:EINZm8oa26LRSVSl@cluster0.t7zaa.mong
         res.sendFile(__dirname + '/index.html')
     })
 
-    app.get('/js/main.js', (req, res) => {
-        res.sendFile(__dirname + '/js/main.js')
-    })
-
     app.get('/add', (req, res) => {
         res.sendFile(__dirname + '/add.html')
-    })
-
-    app.get('/js/main.js', (req, res) => {
-        res.sendFile(__dirname + '/js/main.js')
-    })
-
-    app.get('/css/style.css', (req, res) => {
-        res.sendFile(__dirname + '/css/style.css') //being lazy and only using one stylesheet for all pages
     })
 
     app.get('/randomPasta', (req, res) => {
